ff37119c248559f5bac568eddf8a7d8e2ef4b065
Leo <le4ndro.fioretti@gmail.com>
2025-07-02 02:16:39 +0200

    small fix

--- Solitario/Activities/Rendering/ComponentRenderer.cs
+++ Solitario/Activities/Rendering/ComponentRenderer.cs
@@ -4,48 +4,49 @@ using Solitario.Utils;
 
 namespace Solitario.Activities.Rendering;
 internal static class ComponentRenderer {
-  /// <summary>
-  /// Restituisce una stringa contenente la grafica di un bottone
-  /// </summary>
-  /// <param name="button">Bottone</param>
-  /// <param name="selected">Se deve essere restituita una versione in cui il bottone viene selezionato o no</param>
-  /// <returns></returns>
-  internal static string GetButtonArt(Button button, bool selected) {
-    string art;
-    if (!selected) {
-      var btnText = $" {button.Text} ";
-      art = $@"┌{new string('─', btnText.Length + 2)}┐
+	/// <summary>
+	/// Restituisce una stringa contenente la grafica di un bottone
+	/// </summary>
+	/// <param name="button">Bottone</param>
+	/// <param name="selected">Se deve essere restituita una versione in cui il bottone viene selezionato o no</param>
+	/// <returns></returns>
+	internal static string GetButtonArt(Button button, bool selected) {
+		string art;
+		if (!selected) {
+			var btnText = $" {button.Text} ";
+			art = $@"┌{new string('─', btnText.Length + 2)}┐
 │ {btnText} │
 └{new string('─', btnText.Length + 2)}┘";
-    }
-    else {
-      var selectedBtnText = $"<{button.Text}>";
-      art = $@"{AnsiColors.Foreground.BoldGreen}╔{new string('═', selectedBtnText.Length + 2)}╗
+		}
+		else {
+			var selectedBtnText = $"<{button.Text}>";
+			art = $@"{AnsiColors.Foreground.BoldGreen}╔{new string('═', selectedBtnText.Length + 2)}╗
 ║ {AnsiColors.Background.DarkGreen}{AnsiColors.Foreground.Lime}{selectedBtnText}{AnsiColors.Reset} {AnsiColors.Foreground.BoldGreen}║
 ╚{new string('═', selectedBtnText.Length + 2)}╝{AnsiColors.Reset}";
-    }
+		}
 
-    return art;
-  }
+		return art;
+	}
 
-  /// <summary>
-  /// Restituisce una stringa contenente la grafica della checkbox
-  /// </summary>
-  /// <param name="checkbox">L'oggetto della checkbox</param>
-  /// <param name="selected">Se deve essere restituita una versione in cui la checkbox viene selezionato o no</param>
-  /// <returns></returns>
-  internal static string GetCheckboxArt(Checkbox checkbox, bool selected) {
-    string icon;
-    if (CurrentSettings.UseAnsi) {
-      icon = checkbox.Checked ? "🗹" : "☐";
-    }
-    else {
-      icon = checkbox.Checked ? "[x]" : "[ ]";
-    }
+	/// <summary>
+	/// Restituisce una stringa contenente la grafica della checkbox
+	/// </summary>
+	/// <param name="checkbox">L'oggetto della checkbox</param>
+	/// <param name="selected">Se deve essere restituita una versione in cui la checkbox viene selezionato o no</param>
+	/// <returns></returns>
+	internal static string GetCheckboxArt(Checkbox checkbox, bool selected) {
+		string icon;
+		string extraChar = Console.WindowWidth % 2 == 0 ? "" : " "; // Previene problemi di visualizzazione
+		if (CurrentSettings.UseAnsi) {
+			icon = checkbox.Checked ? "🗹" : "☐";
+		}
+		else {
+			icon = checkbox.Checked ? "[x]" : "[ ]";
+		}
 
-    if (!selected)
-      return $"  {checkbox.Text} {icon} ";
+		if (!selected)
+			return $"  {checkbox.Text} {icon} ";
 
-    return $"{AnsiColors.Background.DarkGreen}> {checkbox.Text} {icon} {AnsiColors.Reset}";
-  }
+		return $"{AnsiColors.Background.DarkGreen}> {checkbox.Text} {icon}{extraChar} {AnsiColors.Reset}";
+	}
 }
--- Solitario/Activities/Screens/SettingsActivity.cs
+++ Solitario/Activities/Screens/SettingsActivity.cs
@@ -5,88 +5,92 @@ using Solitario.Utils;
 
 namespace Solitario.Activities.Screens;
 internal class SettingsActivity : IActivity {
-  private static readonly ActionComponent[] baseComponents = new ActionComponent[3];
-  private readonly ActionComponent[] _elements = baseComponents;
-  private readonly ActivityManager _activityManager;
+	private static readonly ActionComponent[] baseComponents = new ActionComponent[3];
+	private readonly ActionComponent[] _elements = baseComponents;
+	private readonly ActivityManager _activityManager;
 
-  private int _selectedIndex = 1;
+	private int _selectedIndex = 1;
 
-  private readonly Checkbox shuffle_cb, useHints_cb;
+	private readonly Checkbox shuffle_cb, useHints_cb;
 
-  internal SettingsActivity(ActivityManager activityManager) {
-    this._activityManager = activityManager;
+	internal SettingsActivity(ActivityManager activityManager) {
+		this._activityManager = activityManager;
 
-    var backBtn = new Button("◄ Indietro (Esc)", () => {
-      activityManager.Back();
-    });
-    shuffle_cb = new Checkbox("Rimescola carte quando il mazzo si svuota ", () => {
-      CurrentSettings.ShuffleEmptyDeck = shuffle_cb!.Checked;
-      DrawUI();
-    });
-    useHints_cb = new Checkbox("Abilita suggerimenti", () => {
-      CurrentSettings.UseHints = useHints_cb!.Checked;
-      DrawUI();
-    });
+		var backBtn = new Button("◄ Indietro (Esc)", () => {
+			activityManager.Back();
+		});
+		shuffle_cb = new Checkbox("Rimescola carte quando il mazzo si svuota ", () => {
+			CurrentSettings.ShuffleEmptyDeck = shuffle_cb!.Checked;
+			DrawUI();
+		});
+		useHints_cb = new Checkbox("Abilita suggerimenti", () => {
+			CurrentSettings.UseHints = useHints_cb!.Checked;
+			DrawUI();
+		});
 
-    _elements[0] = backBtn;
-    _elements[1] = shuffle_cb;
-    _elements[2] = useHints_cb;
-  }
+		_elements[0] = backBtn;
+		_elements[1] = shuffle_cb;
+		_elements[2] = useHints_cb;
+	}
 
-  public void OnEnter() {
-    shuffle_cb.Checked = CurrentSettings.ShuffleEmptyDeck;
-    useHints_cb.Checked = CurrentSettings.UseHints;
+	public void OnEnter() {
+		shuffle_cb.Checked = CurrentSettings.ShuffleEmptyDeck;
+		useHints_cb.Checked = CurrentSettings.UseHints;
 
-    //Draw();
-  }
+		//Draw();
+	}
 
-  public void HandleInput(ConsoleKeyInfo keyInfo) {
-    switch (keyInfo.Key) {
-      case ConsoleKey.Escape:
-        _activityManager.Back();
-        break;
+	public void HandleInput(ConsoleKeyInfo keyInfo) {
+		switch (keyInfo.Key) {
+			case ConsoleKey.Escape:
+				_activityManager.Back();
+				break;
 
-      case ConsoleKey.UpArrow:
-        if (_selectedIndex <= 0) break;
-        _selectedIndex--;
-        DrawUI();
-        break;
-      case ConsoleKey.DownArrow:
-        if (_selectedIndex >= _elements.Length - 1) break;
-        _selectedIndex++;
-        DrawUI();
-        break;
+			case ConsoleKey.UpArrow:
+				if (_selectedIndex <= 0) break;
+				_selectedIndex--;
+				DrawUI();
+				break;
+			case ConsoleKey.DownArrow:
+				if (_selectedIndex >= _elements.Length - 1) break;
+				_selectedIndex++;
+				DrawUI();
+				break;
 
-      case ConsoleKey.Spacebar:
-      case ConsoleKey.Enter:
-        if (_elements[_selectedIndex] is Checkbox checkbox) {
-          checkbox.Toggle();
-        }
-        _elements[_selectedIndex]?.OnClick();
-        break;
-    }
-  }
+			case ConsoleKey.Spacebar:
+			case ConsoleKey.Enter:
+				if (_elements[_selectedIndex] is Checkbox checkbox) {
+					checkbox.Toggle();
+				}
+				_elements[_selectedIndex]?.OnClick();
+				break;
+		}
+	}
 
-  public void Draw() {
-    Console.Clear();
-    DrawUI();
-  }
+	public void Draw() {
+		Console.Clear();
+		DrawUI();
+	}
 
-  public (int, int) GetMinSize() {
-    return (46, 20);
-  }
+	public (int, int) GetMinSize() {
+		return (49, 20);
+	}
 
-  private void DrawUI() {
-    const int startY = 5;
-    const int spacing = 2;
+	private void DrawUI() {
+		const int startY = 5;
+		const int spacing = 2;
 
-    Pencil.DrawArt(ComponentRenderer.GetButtonArt((Button)_elements[0], _selectedIndex == 0), 1, 0);
+		Pencil.DrawArt(ComponentRenderer.GetButtonArt((Button)_elements[0], _selectedIndex == 0), 1, 0);
 
-    for (int i = 1; i < _elements.Length; i++) {
-      var el = _elements[i];
-      if (el != null) {
-        Pencil.DrawCentered(ComponentRenderer.GetCheckboxArt((Checkbox)el, _selectedIndex == i), startY + (i * spacing));
-      }
-    }
-  }
+		for (int i = 1; i < _elements.Length; i++) {
+			var el = _elements[i];
+			if (el != null) {
+				// Queste due linee prevengono eventuali problemi di visualizzazione.
+				Console.SetCursorPosition(0, startY + (i * spacing));
+				Console.Write(new string(' ', Console.WindowWidth));
+
+				Pencil.DrawCentered(ComponentRenderer.GetCheckboxArt((Checkbox)el, _selectedIndex == i), startY + (i * spacing));
+			}
+		}
+	}
 }
